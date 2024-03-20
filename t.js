c5fbe5cb35d920291b7cbcfb906206ba5dec62d4
seatop <seatop@126.com>
2024-03-20 09:19:04 +0800

    update

--- t.js
+++ t.js
@@ -1,13 +1,13 @@
 let rchar = function(){
-		let st = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+:\"\<\>?-=\;\'.\\\/[]{}';
-		return st.charAt(Math.round(Math.random()*st.length-1));
+		let st = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+:?[]{}';
+		return st.charAt(Math.floor(Math.random()*st.length));
 	}
 
 function T(){
 	let theid = document.getElementById('theid');
 	let thehtml = '';	
 	let wn = Math.floor(window.innerWidth/20)-1;
-	let hn = Math.floor(window.innerHeight/24);
+	let hn = Math.floor(window.innerHeight/21);
 	for(let i =0;i<hn;i++){
 		for(let j = 0;j<wn;j++){
 			thehtml += '\<span class="schar" id=\"s_'+i+'_'+j+'"\>'+rchar()+'\<\/span\>';
@@ -19,19 +19,16 @@ function T(){
 
 function C(){
 	let wn = Math.floor(window.innerWidth/20)-1;
-	let hn = Math.floor(window.innerHeight/24);
-
+	let hn = Math.floor(window.innerHeight/21);
+	let j =Math.floor(Math.random()*wn);
 	
     for(let i=hn-1;i>0;i--){
     	let ii=i-1;
-    	for(let k=0;k<wn;k++){			
-			document.getElementById("s_"+i+"_"+k).innerText = document.getElementById("s_"+ii+"_"+k).innerText;		
-		}
-
+    	document.getElementById("s_"+i+"_"+j).innerText = document.getElementById("s_"+ii+"_"+j).innerText;
     }
-    for(let j=0;j<wn;j++){			
-		document.getElementById("s_0_"+j).innerText = rchar();		
-	}
+    
+	document.getElementById("s_0_"+j).innerText = rchar();		
+	
 }
 
 T();
@@ -41,4 +38,4 @@ window.onresize=function(){
 	T();
 
 }
-setInterval("C()",5);
+setInterval("C()",50);
